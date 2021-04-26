c5f6cc0573e2da33b6800fb7dfeb438f5c2d0e5a
ForHack <olimjonov-odiljon@mail.ru>
2021-04-26 14:34:17 +0500

    Index page of Admin panel is ready. CSS and JS work

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,23 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+
+mix.styles([
+    'resources/assets/admin/css/material-dashboard.css',
+    'resources/assets/admin/demo/demo.css',
+], 'public/assets/admin/css/admin.css')
+
+mix.scripts([
+    'resources/assets/admin/js/core/jquery.min.js',
+    'resources/assets/admin/js/core/popper.min.js',
+    'resources/assets/admin/js/core/bootstrap-material-design.min.js',
+    'resources/assets/admin/js/core/bootstrap-material-design.min.js',
+    'resources/assets/admin/js/plugins/perfect-scrollbar.jquery.min.js',
+    'resources/assets/admin/js/plugins/chartist.min.js',
+    'resources/assets/admin/js/material-dashboard.js',
+    'resources/assets/admin/demo/demo.js',
+], 'public/assets/admin/js/admin.js')
+
+
+mix.copyDirectory('resources/assets/admin/img/', 'public/assets/admin')
